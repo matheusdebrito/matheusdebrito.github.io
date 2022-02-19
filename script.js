495fff3f175e4ffda2189412d89254fe12e08f4a
matheusdebrito <matheusdebrito81@gmail.com>
2022-02-19 14:15:16 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("Em desenvolvimento");
+//alert("Em Desenvolvimento !");
+
+//funções para ao clickar no menu ir para o link de forma suave
+const menuItems = document.querySelectorAll("#nav a");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", scroollToIdOnClick);
+})
+
+function scroollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 100;
+    scrollToPosition(to)
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior: "smooth"
+    });   
+}
+
+//fim da função
